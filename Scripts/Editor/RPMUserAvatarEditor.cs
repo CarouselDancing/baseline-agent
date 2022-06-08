5e64f4ed8e05755ba08c27ca5fa51c2ceaed6c18
Erik Herrmann <Erik.Herrmann@dfki.de>
2022-06-08 15:55:56 +0200

    Added missing prefabs and editor for user avatar

---
+++ Scripts/Editor/RPMUserAvatarEditor.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Carousel
+{
+    namespace BaselineAgent{
+[CustomEditor(typeof(RPMUserAvatar))]
+public class RPMUserAvatarEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        var controller = (RPMUserAvatar)target;
+
+        if (GUILayout.Button("Generate"))
+        {
+            controller.CreateDancer();
+        }   
+        /*
+        if (GUILayout.Button("ActivateAgent"))
+        {
+            controller.ActivateAgent();
+        }
+        if (GUILayout.Button("DeactivateAgent"))
+        {
+            controller.DeactivateAgent();
+        } 
+        if (GUILayout.Button("ActivateFollower"))
+        {
+            controller.ActivateFollower();
+        }
+         if (GUILayout.Button("DeactivateFollower"))
+        {
+            controller.DeactivateFollower();
+        }
+        if (GUILayout.Button("ActivatePairDance"))
+        {
+            controller.ActivatePairDance();
+        }
+         if (GUILayout.Button("DeactivatePairDance"))
+        {
+            controller.DeactivatePairDance();
+        }
+        if (GUILayout.Button("ToggleAgentDance"))
+        {
+            controller.ToggleAgentDancing();
+        }*/
+
+    }
+
+}
+}
+}
