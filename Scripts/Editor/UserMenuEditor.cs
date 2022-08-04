cbd5ed21c557543d7164dfb9c850699608ee6777
Erik Herrmann <Erik.Herrmann@dfki.de>
2022-08-04 14:28:14 +0200

    Added editor buttons for usermenu

---
+++ Scripts/Editor/UserMenuEditor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Carousel
+{
+namespace BaselineAgent{
+[CustomEditor(typeof(UserMenu))]
+public class UserMenuEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        var controller = (UserMenu)target;
+        if (GUILayout.Button("ToggleFollower"))
+        {
+            controller.ToggleFollower();
+        }   
+        if (GUILayout.Button("ToggleMirror"))
+        {
+            controller.ToggleMirror();
+        }   
+        if (GUILayout.Button("SetHeight"))
+        {
+            controller.SetHeight();
+        }   
+        if (GUILayout.Button("ExitGame"))
+        {
+            controller.ExitGame();
+        }
+
+    }
+
+}
+}
+}
