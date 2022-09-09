f1eeaa312f253e240713b5eefee9215eb15ff589
Erik Herrmann <erik.herrmann@dfki.de>
2022-09-09 19:27:51 +0200

    Fixed random start position

--- Scripts/Spawner/AgentSpawnZone.cs
+++ Scripts/Spawner/AgentSpawnZone.cs
@@ -10,10 +10,12 @@ public class AgentSpawnZone : MonoBehaviour
         public Vector3 GetRandomStartPosition()
     {
         var bc = GetComponent<BoxCollider>();
-        float xo = Random.Range(-bc.size[0],bc.size[0])/2f;
-        float yo = -bc.size[1] /2;
-        float zo = Random.Range(-bc.size[2],bc.size[2])/2f;
-        return transform.position + new Vector3(xo, yo, zo);
+        var size = bc.bounds.size;
+        float xo = Random.Range(-size[0],size[0])/2f;
+        float yo = -size[1] /2;
+        float zo = Random.Range(-size[2],size[2])/2f;
+        var pos = new Vector3(xo, yo, zo);
+        return transform.position + pos;
     }
 
 }
